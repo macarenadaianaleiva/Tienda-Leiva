9d0d670cdc6b8e8c34933a91b046f4c6c32a0675
Macarena Leiva <Macaleiva_93@hotmail.com>
2022-07-20 19:44:47 -0300

    componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+export const NavBar = () =>{
+    return(
+        <nav>
+            <h1>LOGO</h1>
+            <ul>
+                <li><a href=''>Auiculares</a></li>
+                <li><a href=''>Teclados</a></li>
+                <li><a href=''>Mouse</a></li>
+                <li><a href=''>Sillas</a></li>
+                <li><a href=''>LOGIN</a></li>
+                
+            </ul>
+        </nav>
+    )
+}
